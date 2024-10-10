1a0cfc05b52717dfade6d2278bca4c50a05ea6b4
Carlos Ronyhelton <carlosronyhelton@hotmail.com>
2024-10-10 14:34:57 -0300

    Aula01

---
+++ Aula 10 10/aula01.m
@@ -0,0 +1,10 @@
+clear; clc;
+n = input('Lin: ');
+m = input('Col: ');
+for lin = 1:n
+    for col = 1:m
+        fprintf('Pedido mat(%d,%d): ',lin,col);
+        mat(lin,col) = input(' ');
+    end
+end
+disp(mat)
